97562b2ce783ec1d8fede44812f20ccc264c9e5a
emreissvr <80003871+emreissvr@users.noreply.github.com>
2022-03-15 23:14:20 +0300

    CRUD Operation and MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,64 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require("ejs");
+const path = require('path');
+const Photo = require("./models/Photo");
+
+
+
+
+const app = express();
+
+
+// connect DB
+mongoose.connect("mongodb://localhost/pcat-test-db",{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+
+// template engine 
+app.set("view engine", "ejs");
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+// routes
+app.get('/', async (req,res) => {
+
+  const photos = await Photo.find({});
+  
+  res.render("index", {
+    photos 
+  });
+
+});
+
+app.get('/about',(req,res) => {
+
+  res.render("about");
+
+});
+
+app.get('/add',(req,res) => {
+
+  
+  res.render("add");
+
+});
+
+
+app.post('/photos', async (req,res) => {
+
+  await Photo.create(req.body);
+  res.redirect("/");
+
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı `);
+});
